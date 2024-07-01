f9c606b39c2c2342db08e38940f6c28dc4c3ecd9
PK1ST <phuwanon_ke62@live.rmutl.ac.th>
2024-07-01 21:00:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://valid.layercode.workers.dev/list/countries?format=select&flags=true&value=code"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCountries(data.countries);
+        setSelectedCountry(data.userSelectValue);
+      });
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen bg-slate-400">
+      <div className="w-screen h-screen  flex justify-center items-center">
+        <div className="w-2/5 max-md:w-full bg-white rounded-md">
+          <div className="p-5">
+            <div className="flex items-center max-md:flex-col">
+              <p className="mr-5 w-72 max-md:w-full max-md:mr-0">First Name:</p>
+              <input
+                type="text"
+                className="w-full outline-none p-2 bg-slate-100 border"
+                placeholder="Your name.."
+              />
+            </div>
+            <div className="flex items-center mt-2 max-md:flex-col">
+              <p className="mr-5 w-72 max-md:w-full max-md:mr-0">Last Name:</p>
+              <input
+                type="text"
+                className="w-full outline-none p-2 bg-slate-100 border"
+                placeholder="Last name.."
+              />
+            </div>
+            <div className="flex items-center mt-2  max-md:flex-col">
+              <p className="mr-5 w-72 max-md:w-full max-md:mr-0">Country:</p>
+              <select
+                className="w-full outline-none p-2 bg-slate-100 border"
+                value={selectedCountry}
+                onChange={handleCountryChange}
+              >
+                {countries.map((country) => (
+                  <option key={country.value} value={country.value}>
+                    {country.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex mt-2 max-md:flex-col">
+              <p className="mr-5 w-72">Subject:</p>
+              <textarea placeholder="Write something..." className="w-full outline-none p-2 h-72 bg-slate-100 border"></textarea>
+            </div>
+          </div>
+          <div className="p-1 flex justify-end items-center">
+            <button className="p-2 text-white bg-emerald-500 rounded-md">
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
